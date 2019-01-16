11b90dff3f60ca28d779e54490efd240bb11207d
Rachit Goel <rachit18csu098@ncuindia.edu>
2019-01-16 09:50:31 +0530

    Focp2

---
+++ Focp2/datatypes/src/demoint.java
@@ -0,0 +1,17 @@
+class demoint
+{
+	public static void main(String args[])
+	{
+		int a=1,b=4,add,sum,mul,div,mod;
+		add=a+b;
+		sum=b-a;
+		mul=a*b;
+		div=a/b;
+		mod=b%a;
+		System.out.println(add);
+		System.out.println(sum);
+		System.out.println(mul);
+		System.out.println(div);
+		System.out.println(mod);
+    }
+}    
